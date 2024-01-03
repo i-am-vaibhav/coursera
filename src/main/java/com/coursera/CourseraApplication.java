40e764e9076b28894733a1c6e9e27b8e06fcdef5
i-am-vaibhav <vg566556@gmail.com>
2024-01-03 22:13:29 +0530

    #16 Removing unused imports

--- src/main/java/com/coursera/CourseraApplication.java
+++ src/main/java/com/coursera/CourseraApplication.java
@@ -2,9 +2,7 @@ package com.coursera;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
