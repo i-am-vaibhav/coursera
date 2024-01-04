2a960d07fbc162cd149cb16054e54b1905719858
i-am-vaibhav <vg566556@gmail.com>
2024-01-04 20:55:22 +0530

    Added encryption for username passwords

--- src/main/java/com/coursera/CourseraApplication.java
+++ src/main/java/com/coursera/CourseraApplication.java
@@ -1,11 +1,13 @@
 package com.coursera;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
+@EnableEncryptableProperties
 public class CourseraApplication {
 
     public static void main(String[] args) {
