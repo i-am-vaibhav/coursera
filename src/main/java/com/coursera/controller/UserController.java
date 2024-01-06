537419bd504993d79b9b459b0174f7686f9fdefd
i-am-vaibhav <vg566556@gmail.com>
2024-01-06 20:33:04 +0530

    #14 Backend changes to change password

--- src/main/java/com/coursera/controller/UserController.java
+++ src/main/java/com/coursera/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.math.BigDecimal;
 import java.util.Optional;
